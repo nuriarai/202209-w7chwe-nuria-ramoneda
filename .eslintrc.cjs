b4dd21d496041625b9ff9297e65a0f134768c274
Núria Ramoneda <nuriarai@gmail.com>
2022-11-11 21:57:40 +0100

    Add chalk types and eslint config for tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,10 +18,18 @@ module.exports = {
         "@typescript-eslint/no-empty-function": "off",
       },
     },
+    {
+      files: ["src/**/models/**/*.ts"],
+      rules: {
+        "@typescript-eslint/naming-convention": "off",
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "no-implicit-coercion": "off",
+  },
 };
