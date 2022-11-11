dd6f311b04792be04a38a0bae799117cac217322
Núria Ramoneda <nuriarai@gmail.com>
2022-11-11 21:58:19 +0100

    Add call to connect db and server in index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import environtment from "./loadEnvirontment.js";
+import express from "express";
+import startServer from "./server/index.js";
+import connectDb from "./database/index.js";
 
+const app = express();
+const { port, dbUrl } = environtment;
+
+await connectDb(dbUrl);
+await startServer(app, +port);
