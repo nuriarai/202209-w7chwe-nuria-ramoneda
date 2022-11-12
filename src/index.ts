377934347c1d64ad354dc292531a413522ee2637
Núria Ramoneda <nuriarai@gmail.com>
2022-11-12 16:20:32 +0100

    Add usersRouter and app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import "./loadEnvirontment.js";
 import environtment from "./loadEnvirontment.js";
-import express from "express";
 import startServer from "./server/index.js";
 import connectDb from "./database/index.js";
+import app from "./app.js";
 
-const app = express();
 const { port, dbUrl } = environtment;
 
 await connectDb(dbUrl);
