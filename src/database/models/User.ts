5efbb102638690435327f2bdd57622f6ed709b43
Núria Ramoneda <nuriarai@gmail.com>
2022-11-12 12:04:44 +0100

    Add user schema/model

---
+++ src/database/models/User.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  picture: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+export default User;
