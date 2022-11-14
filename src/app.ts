bda8ac5efee479798271e1cb0e9d533f0dbade85
Núria Ramoneda <nuriarai@gmail.com>
2022-11-14 22:30:44 +0100

    Add cors library and add it to app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import morgan from "morgan";
 import { generalError, notFoundError } from "./server/middlewares/error.js";
 import usersRouter from "./server/usersRouters/usersRouters.js";
+import cors from "cors";
 
 const app = express();
 
 app.disable("x-powered-by");
+app.use(cors());
 
 app.use(morgan("dev"));
 app.use(express.json());
