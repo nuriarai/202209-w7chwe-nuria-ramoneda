f0ff2d1bf84e54a34fba9e9efb6b254c231f6c3f
Núria Ramoneda <nuriarai@gmail.com>
2022-11-14 12:39:44 +0100

    Add case duplicate user to registerUser test

--- src/server/controllers/userControllers/userControllers.test.ts
+++ src/server/controllers/userControllers/userControllers.test.ts
@@ -13,7 +13,7 @@ const next = jest.fn();
 
 describe("Given a registerUser controller", () => {
   describe("When it receives a request with an username Maria, a password 12345 & a email maria@maria.cat", () => {
-    test("Then it should be invoked it method status with a code of 201 ", async () => {
+    test("Then it should be invoked it method status with a code of 201 and its method json wiht the data supplied", async () => {
       const user = {
         username: "Maria",
         password: "12345",
@@ -36,6 +36,29 @@ describe("Given a registerUser controller", () => {
           id: userId,
         },
       });
+    });
+  });
+
+  describe("when it receives a request with an existing user,", () => {
+    test("Then it should be invoked its method status with a code of 500 and its mehod json with a message of 'Database error: duplicate key", async () => {
+      const user = {
+        username: "Maria",
+        password: "123456",
+        email: "maria@maria.cat",
+      };
+      const expectedStatus = 201;
+      const expectedMessage = "Database error: duplicate key";
+      const expectedError = new Error(expectedMessage);
+
+      req.body = user;
+      const userId = new mongoose.Types.ObjectId();
+      bcrypt.hash = jest.fn().mockResolvedValue(user.password);
+      User.create = jest.fn().mockRejectedValue(expectedError);
+
+      await registerUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenLastCalledWith(expectedStatus);
+      expect(next).toHaveBeenCalledWith(expectedError);
     });
   });
 });
