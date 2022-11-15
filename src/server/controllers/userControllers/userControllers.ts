381e5d3e10a72f31d833d2d16c104d7098767102
Núria Ramoneda <nuriarai@gmail.com>
2022-11-15 19:08:41 +0100

    Create loginUser controller

--- src/server/controllers/userControllers/userControllers.ts
+++ src/server/controllers/userControllers/userControllers.ts
@@ -1,8 +1,16 @@
+import environtment from "../../../loadEnvirontment.js";
+import type { JwtPayload } from "jsonwebtoken";
+import Jwt from "jsonwebtoken";
 import type { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
-import type { RegisterData } from "../../types.js";
+import type { Credentials, RegisterData } from "../../types.js";
 import User from "../../../database/models/User.js";
 import CustomError from "../../../CustomError/CustomError.js";
+
+interface UserTokenPayload extends JwtPayload {
+  id: string;
+  username: string;
+}
 
 export const registerUser = async (
   req: Request,
@@ -34,6 +42,53 @@ export const registerUser = async (
     }
 
     const customError = new CustomError(errorObject.message, 500, message);
+
+    next(customError);
+  }
+};
+
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { username, password } = req.body as Credentials;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      const error = new CustomError("User not found", 401, "Wrong credentials");
+      next(error);
+      return;
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      const error = new CustomError(
+        "Password incorrect",
+        404,
+        "Wrong credentials"
+      );
+      next(error);
+      return;
+    }
+
+    const tokenPayload: UserTokenPayload = {
+      id: user._id.toString(),
+      username,
+    };
+
+    const token = Jwt.sign(tokenPayload, environtment.secret, {
+      expiresIn: "2d",
+    });
+
+    res.status(200).json({ accessToken: token });
+  } catch (error: unknown) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Login error"
+    );
 
     next(customError);
   }
